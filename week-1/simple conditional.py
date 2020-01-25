51dfad7990a89ac934556dc89ae6b1c371339e37
Dan Bennett <dghbennett47@gmail.com>
2020-01-25 22:16:56 +0000

    chore: rename file

---
+++ week-1/simple conditional.py
@@ -0,0 +1,13 @@
+# this is the first weeks file
+
+# example of conditionals
+
+x = int(input('Enter an integer: '))
+
+if x % 2 == 0:
+    print('')
+    print('Even')
+else:
+    print('')
+    print('Odd')
+print('Done with conditional')
