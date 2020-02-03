376875af940c7140eaecd75a1c7523199aa6e08d
Dan Bennett <dghbennett47@gmail.com>
2020-02-03 23:09:25 +0000

    feat: recursive towers of hanoi function

--- week-2/week 2 snippets part 2.py
+++ week-2/week 2 snippets part 2.py
@@ -3,7 +3,6 @@
 def isEven(number):
     """
     
-
     Parameters
     ----------
     number : int
@@ -17,7 +16,70 @@ def isEven(number):
     print("hi")
     return number % 2 == 0
 
-x = 5
 
-def h(x):
-    x = x + 1
+# example of iteration
+
+def multiplyIteratively(a, b):
+    result = 0
+    while b > 0:
+        result += a
+        b -= 1
+    return result
+
+
+# example of recursion
+
+def multiplyRecursively(a, b):
+    if b == 1:
+        return a
+    else:
+        return a + multiplyRecursively(a, b - 1)
+
+
+# iterative power function
+
+def iterPower(base, exp):
+    '''
+    base: int or float.
+    exp: int >= 0
+ 
+    returns: int or float, base^exp
+    '''
+    result = 1
+    while exp > 0:
+        result *= base
+        exp -= 1
+    return result
+
+
+# recursive power function
+
+def recurPower(base, exp):
+    '''
+    base: int or float.
+    exp: int >= 0
+ 
+    returns: int or float, base^exp
+    '''
+    if exp == 0:
+        return 1
+    else:
+        return base * recurPower(base, exp - 1)
+
+
+
+
+# towers of hanoi code
+
+def printMove(origin, destination):
+    print('Move from ' + str(origin) + ' to ' + str(destination))
+
+def towers(n, origin, destination, spare):
+    if n == 1:
+        printMove(origin, destination)
+    else:
+        towers(n - 1, origin, spare, destination)
+        towers(1, origin, destination, spare)
+        towers(n - 1, spare, destination, origin)
+
+
