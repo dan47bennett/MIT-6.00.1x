1bb441fd337e727e76b376001890aee8fb372668
Dan Bennett <dghbennett47@gmail.com>
2020-02-03 22:25:10 +0000

    chore: create snippets part 2

---
+++ week-2/week 2 snippets part 2.py
@@ -0,0 +1,23 @@
+# introduction of functions
+
+def isEven(number):
+    """
+    
+
+    Parameters
+    ----------
+    number : int
+        a positive integer
+
+    Returns
+    -------
+    True if number is even, False otherwise
+
+    """
+    print("hi")
+    return number % 2 == 0
+
+x = 5
+
+def h(x):
+    x = x + 1
