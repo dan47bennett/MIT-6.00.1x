65e9120045ffdb9a1a4ed58c0a6cba0fe486752c
Dan Bennett <dghbennett47@gmail.com>
2020-02-27 18:20:18 +0000

    feat: add snippets

--- week-3/week 3 snippets.py
+++ week-3/week 3 snippets.py
@@ -21,3 +21,36 @@ def applyToEach(L, f):
 def applyFunctions(L, x):
     for f in L:
         print(f(x))
+
+
+# create dictionary with frequency of each element in an array or word in a song, for example
+def lyricsToFrequencies(lyrics):
+    wordDictionary = {}
+    for word in lyrics:
+        if word in wordDictionary:
+            wordDictionary[word] += 1
+        else:
+            wordDictionary[word] = 1
+    return wordDictionary
+
+
+# find 
+
+sheLovesYou = ['she', 'loves', 'you', 'yeah', 'yeah', 'yeah',
+'she', 'loves', 'you', 'yeah', 'yeah', 'yeah',
+'she', 'loves', 'you', 'yeah', 'yeah', 'yeah',
+
+'you', 'think', "you've", 'lost', 'your', 'love',
+'well', 'i', 'saw', 'her', 'yesterday-yi-yay',
+"it's", 'you', "she's", 'thinking', 'of',
+'and', 'she', 'told', 'me', 'what', 'to', 'say-yi-yay',
+
+'she', 'says', 'she', 'loves', 'you',
+'and', 'you', 'know', 'that', "can't", 'be', 'bad',
+'yes', 'she', 'loves', 'you',
+'and', 'you', 'know', 'you', 'should', 'be', 'glad',
+]
+
+beatles = lyricsToFrequencies(sheLovesYou)
+
+print(beatles)
